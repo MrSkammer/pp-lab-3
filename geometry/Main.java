356a9f5f88c989463e221deffe3640e9d3c08489
MrSkammer <162018022+MrSkammer@users.noreply.github.com>
2024-04-07 11:54:53 +0200

    2

--- geometry/Main.java
+++ geometry/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("Properties of the circle:");
         System.out.println("Middle of the circle: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
         System.out.println("Radius of the circle: " + circle.getRadius());
+        System.out.println("Area of the circle: " + circle.getArea());
         System.out.println("Circumference of a circle: " + circle.calculatePerimeter());
     }    
 }
