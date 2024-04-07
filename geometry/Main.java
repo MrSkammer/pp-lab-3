08dff6c07cd53da5af286337cfd0787124f9a5ca
Kamil <mrkamkam2053@gmail.com>
2024-04-07 11:11:18 +0200

    3

--- geometry/Main.java
+++ geometry/Main.java
@@ -7,10 +7,13 @@ public class Main {
 
         Circle circle = new Circle(point, 5.0);
 
+        ColoredCircle coloredCircle = new ColoredCircle(point, 5.0, "blue");
+
         System.out.println("Properties of the circle:");
         System.out.println("Middle of the circle: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
         System.out.println("Radius of the circle: " + circle.getRadius());
         System.out.println("Circumference of a circle: " + circle.calculatePerimeter());
+        System.out.println("Kolor koła: " + coloredCircle.getColor());
     }    
 }
 
