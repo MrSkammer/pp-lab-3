4f0713df134438789fe69d99875b13913c2c668a
Kamil <mrkamkam2053@gmail.com>
2024-04-07 11:22:58 +0200

    4

--- geometry/Main.java
+++ geometry/Main.java
@@ -2,18 +2,22 @@ package geometry;
 
 public class Main {
     public static void main(String[] args) {
+        // Tworzenie tablicy obiektów typu Circle i ColoredCircle
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(new Point(1.0, 1.0), 3.0);
+        circles[1] = new ColoredCircle(new Point(2.0, 2.0), 4.0, "red");
+        circles[2] = new ColoredCircle(new Point(3.0, 3.0), 5.0, "green");
 
-        Point point = new Point(3.5, 2.0);
+        // Iteracja tablicy i wywołanie metody calculateArea()
+        for (Circle circle : circles) {
+            double area = circle.calculateArea();
+            System.out.println("Pole koła: " + area);
 
-        Circle circle = new Circle(point, 5.0);
-
-        ColoredCircle coloredCircle = new ColoredCircle(point, 5.0, "blue");
-
-        System.out.println("Properties of the circle:");
-        System.out.println("Middle of the circle: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
-        System.out.println("Radius of the circle: " + circle.getRadius());
-        System.out.println("Circumference of a circle: " + circle.calculatePerimeter());
-        System.out.println("Kolor koła: " + coloredCircle.getColor());
-    }    
+            // Jeżeli element tablicy jest instancją ColoredCircle, wywołaj metodę getColor()
+            if (circle instanceof ColoredCircle) {
+                String color = ((ColoredCircle) circle).getColor();
+                System.out.println("Kolor koła: " + color);
+            }
+        }
+    }
 }
-
